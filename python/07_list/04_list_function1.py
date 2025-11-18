6323640464620fdaccad36472d747af63b5cccca
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-18 22:14:13 +0530

    list function part1

---
+++ python/07_list/04_list_function1.py
@@ -0,0 +1,17 @@
+list1=[10,20,30,40,50,60,70,80,90,100]
+# del
+print("List before deletion: ",list1)
+# Insert
+# list1.insert(2,22)
+# del list1[2]
+list2=[20,30]
+# list1.append(list2)
+list1.extend(list2)
+print(list1)
+# print(list1.pop(2))
+# Remove function
+# print(list1.remove(30))
+# Clear
+# list1.clear()
+# list1[2]=34
+print("List after deletion: ",list1)
