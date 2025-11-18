48cd7eac823fa4470ed32e4ba44b406d4efb57f9
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-18 23:19:31 +0530

    list function part2

---
+++ python/07_list/05_list_function2.py
@@ -0,0 +1,8 @@
+list1=[10,20,30,10,40,50,60,70,10,810,90,100]
+# counting=list1.count(10)
+# maxNumber=max(list1)
+# minNumber=min(list1)
+# list1.sort(reverse=True)
+# list1.reverse()
+print(list1.index(10,4))
+
