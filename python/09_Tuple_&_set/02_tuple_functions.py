010614648eb52aff85da9bf863b71146a7d50cf3
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-21 09:08:05 +0530

    Tuple functions

---
+++ python/09_Tuple_&_set/02_tuple_functions.py
@@ -0,0 +1,15 @@
+tuple1=(10,20,344,90,3,10,8,30,40,50,60,70)
+# Min values
+# print(min(tuple1))
+# Another way
+# minimum=min(tuple1)
+# print(minimum)
+
+# Max values
+# print(max(tuple1))
+# Another way
+# maximum=max(tuple1)
+# print(maximum)
+
+# print(tuple1.count(10))
+print(tuple1.index(10,1))
