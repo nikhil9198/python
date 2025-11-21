a65ee2c923b3195b8fa8e30ed5ea43b19743fdc0
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-21 08:38:11 +0530

    Nested Dictionary

---
+++ python/08_Dictionary/03_Nested_Dictionary.py
@@ -0,0 +1,13 @@
+students={
+    'John':{'age':34,'education':'BS Computer Science','Roll No':'2024ABC-1','Semester':'1st'},
+    'Vick':{'age':24,'education':'BS Artificial Intelligence','Roll No':'2024ABC-2','Semester':'1st'},
+    'Mark':{'age':28,'education':'BS Software Engineering','Roll No':'2024ABC-3','Semester':'1st'},
+    'Johson':{'age':25,'education':'BS Data Security','Roll No':'2024ABC-4','Semester':'1st'},
+}
+print(students['John']['age'])
+# for k,v in students.items():
+#     print(k,v['age'],v['education'])
+
+# students['John']['age']=30
+# for k,v in students.items():
+#     print(k,v)
