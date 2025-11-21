d03062b544bd410e2a33c2604f48b1a298582d18
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-21 09:24:29 +0530

    Understanding Set and set functions

---
+++ python/09_Tuple_&_set/03_Understanding_set.py
@@ -0,0 +1,16 @@
+set1={10,20,30,453,60,784,90}
+# print(set1)
+# for i in set1:
+#     print(i)
+
+print(set1)
+# set1.remove(10)
+#set1.remove(11) # it will give error because 11 is not present
+# set1.discard(11) # it does not give error
+# set1.pop()
+# set1.pop()
+# set1.clear()
+# set1.add(49)
+list1={10, 90,30,40,50,45}
+set1.update(list1)
+print(set1)
