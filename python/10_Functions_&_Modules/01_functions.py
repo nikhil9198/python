f0b06901e0641438017547671fa8c999f7b369c0
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-22 06:39:56 +0530

    functions

---
+++ python/10_Functions_&_Modules/01_functions.py
@@ -0,0 +1,19 @@
+# def showMsg(name="World", language="Python"):
+#     print("Hello",name)
+#     print("We are learning",language)
+#     print("Good luck")
+#     print("")
+# showMsg("Nikhil","C++")
+# showMsg("Rahul","react")
+# showMsg("Abhi","kotlin")
+
+def sum(a,b):
+    c=a+b
+    # print(c)
+    return c
+result=sum(20,40)
+print(result)
+
+# a=int(input("Enter your value: ",))
+# b=int(input("Enter your value: ",))
+# sum(a,b)
