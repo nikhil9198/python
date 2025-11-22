f4c1714392e1033d7ae69613efd3a29346279fd6
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-22 06:40:53 +0530

    Math Module

---
+++ python/10_Functions_&_Modules/03_math_module.py
@@ -0,0 +1,13 @@
+import math
+num1=10.5
+num2=11.5
+num3=-12.7
+num4=8
+list1={10, 3, 50,9,80}
+num5=125
+print("Ceil : ",math.ceil(num1)) # floating value convert into upper integer number
+print("Floor : ",math.floor(num2)) # floating value convert into lower inter number
+print("Fabs : ",math.fabs(num3)) # it will return absolute value
+print("Factorial : ",math.factorial(num4)) # it wil gives factorial of given value
+print("fsum : ",math.fsum(list1))
+print("Sqrt : ",math.sqrt(num5))
