7249a3b17019cec539c1e1dbf75c1a8c10efc373
nikhil9198 <knikhilpatel7281@gmail.com>
2025-11-22 06:40:26 +0530

    Module

---
+++ python/10_Functions_&_Modules/02_Module.py
@@ -0,0 +1,9 @@
+import mymodule as mm
+addition=mm.sum(20,10)
+subtraction=mm.subtract(60,25)
+multiplication=mm.multiply(30,8)
+division=mm.divide(30,6)
+print("sum is :", addition)
+print("Subtraction is :", subtraction)
+print("Multiplication is : ",multiplication)
+print("Division is : ",division)
