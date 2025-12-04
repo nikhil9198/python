359531ccacd895e1cf6b800c1e21d8b6b1b61335
nikhil9198 <knikhilpatel7281@gmail.com>
2025-12-04 21:08:20 +0530

    data scraping updated

--- python/Project/01_data_scraping.py
+++ python/Project/01_data_scraping.py
@@ -9,15 +9,19 @@ soup=BeautifulSoup(req.text, "html.parser")
 
 productsCards=soup.find_all("div", class_="col-md-4 col-xl-4 col-lg-4")
 # print(len(productsCards))
-
-titles=soup.find_all("a", class_="title")
-# print(titles)
-
-for item in titles:
-    print(item.txt)
+#
+# titles=soup.find_all("a", class_="title")
+# # print(titles)
+#
+# for item in titles:
+#     print(item.txt)
 
 prices=soup.find_all("h4", class_="price float-end card-title pull-right")
 # print(titles)
+for item in prices:
+    print(item.txt)
 
+descriptions=soup.find_all("p", class_="price float-end card-title pull-right")
+# print(titles)
 for item in prices:
     print(item.txt)
