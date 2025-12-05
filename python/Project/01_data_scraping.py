b7592cd1f66a38e3a57435f1129b66e9159923be
nikhil9198 <knikhilpatel7281@gmail.com>
2025-12-05 22:23:30 +0530

    data scraping updated

--- python/Project/01_data_scraping.py
+++ python/Project/01_data_scraping.py
@@ -7,9 +7,10 @@ req=requests.get(url)
 soup=BeautifulSoup(req.text, "html.parser")
 # print(soup)
 
-productsCards=soup.find_all("div", class_="col-md-4 col-xl-4 col-lg-4")
+[item.text,strip() for item in soup.find_all("div", class_="col-md-4 col-xl-4 col-lg-4")]
+productsCards=
 # print(len(productsCards))
-#
+
 # titles=soup.find_all("a", class_="title")
 # # print(titles)
 #
@@ -17,11 +18,13 @@ productsCards=soup.find_all("div", class_="col-md-4 col-xl-4 col-lg-4")
 #     print(item.txt)
 
 prices=soup.find_all("h4", class_="price float-end card-title pull-right")
-# print(titles)
-for item in prices:
-    print(item.txt)
+# for item in prices:
+#     print(item.txt)
 
-descriptions=soup.find_all("p", class_="price float-end card-title pull-right")
-# print(titles)
-for item in prices:
+descriptions=soup.find_all("p", class_="description card-text")
+# for item in descriptions:
+#     print(item.txt)
+
+noOfReviews=soup.find_all("p", class_="review-count float-end")
+for item in noOfReviews:
     print(item.txt)
